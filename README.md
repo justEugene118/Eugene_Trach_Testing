e6de1db288513ac5e6163d119b3d459d6deab453
Eugene <trach.eugene@lll.kpi.ua>
2022-12-12 02:17:27 +0200

    fit: added WebAPI basic homework

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# Eugene_Trach_Testing
+# Web API homework
+
+In this homework I am uploading [test.txt](test_files/test.txt) file to dropbox, getting its metadata, and then deleting it
+
+Steps to start a project:
+1. After cloning a repository WebAPI branch run `npm i`, which will download all the missing packages
+2. Run `tsc`, to create .js files of .ts
+3. Run `npm test` to run a WebAPI tests
+
+
+Stack used in this homework:
+1. [DropBox API](https://www.dropbox.com/developers/documentation/http/documentation) and its wrapper [dropbox-v2-api](https://www.npmjs.com/package/dropbox-v2-api/v/2.3.0)
+2. [Mocha](https://mochajs.org) test reporter
+3. [Typescript](https://www.typescriptlang.org/docs/)
---
+++ index.ts
@@ -0,0 +1,62 @@
+import {rejects} from "assert";
+
+const dropboxV2Api = require('dropbox-v2-api');
+const fs = require('fs')
+
+const dropbox = dropboxV2Api.authenticate({
+    token: 'sl.BUxpfW73hHRsLOXRVnCq-BWpzZvx-qK7IvDXGRIX3VhBZdoeIvcRkbrxWvMz7TLsDrGBJy4fyUcAndCi426YtjWFnGuFi_dSm7r5i2UtlQ_9y1-CTHe9ayGlatmycYL7GUWRdmI6E0VU'
+});
+
+export const UploadFile = (file_dir: string, file_name: string, dbx_dir: string): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        dropbox({
+            resource: 'files/upload',
+            parameters: {
+                path: dbx_dir + file_name
+            },
+            readStream: fs.createReadStream(file_dir + file_name)
+        }, (err, result, response) => {
+            if (err) {
+                console.log(err);
+                reject(false);
+            }
+            resolve(true);
+        });
+    })
+}
+
+export const FileMetadata = (dbx_dir: string, file_name: string): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        dropbox({
+            resource: 'files/get_metadata',
+            parameters: {
+                path: dbx_dir + file_name,
+                include_media_info: false
+            }
+        }, (err, result, response) => {
+            if (err) {
+                console.log(err);
+                reject(false);
+            }
+            console.log(result);
+            resolve(true)
+        });
+    })
+}
+
+export const  DeleteFile = (dbx_dir: string, file_name: string): Promise<boolean> => {
+    return new Promise<boolean>((resolve, reject) => {
+        dropbox({
+            resource: 'files/delete',
+            parameters: {
+                path: dbx_dir + file_name
+            }
+        }, (err, response) => {
+            if (err) {
+                console.log(err);
+                reject(false);
+            }
+            resolve(true);
+        })
+    })
+}
